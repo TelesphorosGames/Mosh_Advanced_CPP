790c3fcbd2770a2e4597e14335a82745fe422e7d
Colin Severing <telesphorosvids@gmail.com>
2022-09-29 14:05:13 -0600

    finished advanced course!

---
+++ ConsoleApplication1/Pair.h
@@ -0,0 +1,58 @@
+﻿#pragma once
+#include <string>
+
+
+using namespace std;
+
+template<typename K, typename V>
+class Pair
+{
+public:
+     Pair() = default;
+     Pair(const K& key, const V& value);
+    
+     K key() const;
+     void set_key(const K& key);
+     V value() const;
+     void set_value(const V& value);
+
+
+private:
+    
+    K Key;
+    V Value{};
+
+   
+};
+
+template<typename K, typename V>
+Pair<K,V>::Pair(const K& key, const V& value)
+          : Key(key),
+            Value(value)
+{
+    
+}
+
+template<typename K, typename V>
+K Pair<K,V>::key() const
+{
+    return Key;
+}
+
+template<typename K, typename V>
+void Pair<K,V>::set_key(const K& key)
+{
+    Key = key;
+}
+
+template<typename K, typename V>
+V Pair<K,V>::value() const
+{
+    return Value;
+}
+
+template<typename K, typename V>
+void Pair<K,V>::set_value(const V& value)
+{
+    Value = value;
+}
